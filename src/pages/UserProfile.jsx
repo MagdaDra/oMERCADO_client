5c8639869c7946e29505bb634fa8e03b03f545ee
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-08-10 17:06:23 +0100

    Get user details is working correctly

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,38 +1,55 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
-import {AuthContext} from '../context/auth.context'
-
+import { AuthContext } from '../context/auth.context';
 
 const UserProfile = () => {
 	const [servicesOffered, setServicesOffered] = useState([]);
 	const [servicesBought, setServicesBought] = useState([]);
-  const [userDetails, setUserDetails] = useState(null)
-	
+	const [userDetails, setUserDetails] = useState(null);
+
 	const navigate = useNavigate();
 
-  const {user} = useContext(AuthContext)
+	const { user } = useContext(AuthContext);
 
 	const getUserDetails = async () => {
-    try {
-      const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/user/${user._id}`)
-      setUserDetails(response.data)
-      setServicesOffered(response.data.servicesOffered)
-      setServicesBought(response.data.servicesBought)
-      // console.log(`User details:`, response.data)
-      // console.log(`Services offered: `, response.data.servicesOffered)
-      // console.log(`Services bought: `, response.data.servicesBought)
-    } catch (error) {
-      console.error(error)
-    }
-  }
+		try {
+			const response = await axios.get(
+				`${import.meta.env.VITE_API_URL}/api/user/${user._id}`,
+			);
+			setUserDetails(response.data);
+			setServicesOffered(response.data.servicesOffered);
+			setServicesBought(response.data.servicesBought);
+			// console.log(`User details:`, response.data)
+			// console.log(`Services offered: `, response.data.servicesOffered)
+			// console.log(`Services bought: `, response.data.servicesBought)
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  useEffect(() => {
-    getUserDetails();
-  }, [])
+	useEffect(() => {
+		getUserDetails();
+	}, []);
 
+  return(
+    <>
+      <h1>Services Offered</h1>
+      {servicesOffered.map((service) => {
+        return (
+          <div key={service._id}>
+            <Link to={`/services/${service._id}`}>
+							<img className='service-img' src={service.img}></img>
+							<h2>{service.serviceName}</h2>
+						</Link>
+						<p>Price: {service.price} € </p>
+            <p>Quantity: {service.quantity} </p>
+          </div>
+        )
+      })}
 
-
+    </>
+  )
 };
 
 export default UserProfile;
