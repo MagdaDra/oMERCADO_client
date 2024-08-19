9c3694a6516b252523c6cfccec238d6889c55928
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-08-19 15:19:41 +0100

    HomePage styled

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -15,15 +15,15 @@ function Homepage() {
 
 		<div className='homepage-header'>
 			<h1 className='title'><span className='o'>o</span>MERCADO</h1>
-			<p className='header-description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit molestias est deserunt quis hic incidunt ipsa debitis vero laboriosam, at modi error libero repellendus aliquam. Corporis ullam fuga ipsam vero.</p>
+			{/* <p className='header-description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit molestias est deserunt quis hic incidunt ipsa debitis vero laboriosam, at modi error libero repellendus aliquam. Corporis ullam fuga ipsam vero.</p> */}
 		</div>
 
 		<div className='homepage'>
 
 			<div className='category-button art-button' onClick={() => handleCategoryClick('Art')}><span className='art-text'>Art</span></div>
-			<div className='category-button design-button' onClick={() => handleCategoryClick('Design')}>Design</div>
-			<div className='category-button food-button' onClick={() => handleCategoryClick('Food')}>Food</div>
-			<div className='category-button sport-button' onClick={() => handleCategoryClick('Sport')}>Sport</div>
+			<div className='category-button design-button' onClick={() => handleCategoryClick('Design')}><span className='design-text'>Design</span></div>
+			<div className='category-button food-button' onClick={() => handleCategoryClick('Food')}><span className='food-text'>Food</span></div>
+			<div className='category-button sport-button' onClick={() => handleCategoryClick('Sport')}><span className='sport-text'>Sport</span></div>
 			<div className='category-button technology-button' onClick={() => handleCategoryClick('Technology')}>Technology</div>
 			<div className='category-button travel-button' onClick={() => handleCategoryClick('Travel')}>Travel</div>
 			
