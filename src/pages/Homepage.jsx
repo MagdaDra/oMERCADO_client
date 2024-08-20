2a4ece24b4e99cce78eb40da791c3947d8195479
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-08-20 17:08:27 +0100

    Added browse-all button

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,36 +1,66 @@
-import { useNavigate } from "react-router-dom";
-
+import { useNavigate, Link } from 'react-router-dom';
 
 function Homepage() {
-	
 	const navigate = useNavigate();
 
 	const handleCategoryClick = (selectedCategory) => {
-	
-		navigate('/main', {state: {category: selectedCategory}})
-	}
+		navigate('/main', { state: { category: selectedCategory } });
+	};
 
 	return (
 		<>
+			<div className='homepage-header'>
+				<h1 className='title'>
+					<span className='o'>o</span>MERCADO
+				</h1>
+				<div className='description-and-button'>
+					<p className='header-description'>
+						The ultimate marketplace for buying and selling professional
+						services. <br /> Simple, secure, and efficient. <br /> <br /> Join
+						our community today and experience the future of service exchange.
+					</p>
 
-		<div className='homepage-header'>
-			<h1 className='title'><span className='o'>o</span>MERCADO</h1>
-			<p className='header-description'>Welcome to the ultimate marketplace for buying and selling professional services. <br/> Simple, secure, and efficient. <br/> <br/> Join our community today and experience the future of service exchange.</p>
-		</div>
+					<Link to='/main'>
+						<button className='browse-button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>
+							Browse all
+						</button>
+					</Link>
+				</div>
+			</div>
 
-		<div className='homepage'>
-
-			<div className='category-button art-button' onClick={() => handleCategoryClick('Art')}><span className='art-text'>Art</span></div>
-			<div className='category-button design-button' onClick={() => handleCategoryClick('Design')}><span className='design-text'>Design</span></div>
-			<div className='category-button food-button' onClick={() => handleCategoryClick('Food')}><span className='food-text'>Food</span></div>
-			<div className='category-button sport-button' onClick={() => handleCategoryClick('Sport')}><span className='sport-text'>Sport</span></div>
-			<div className='category-button technology-button' onClick={() => handleCategoryClick('Technology')}>Technology</div>
-			<div className='category-button travel-button' onClick={() => handleCategoryClick('Travel')}>Travel</div>
-			
-		</div>
+			<div className='homepage'>
+				<div
+					className='category-button art-button'
+					onClick={() => handleCategoryClick('Art')}>
+					<span className='art-text'>Art</span>
+				</div>
+				<div
+					className='category-button design-button'
+					onClick={() => handleCategoryClick('Design')}>
+					<span className='design-text'>Design</span>
+				</div>
+				<div
+					className='category-button food-button'
+					onClick={() => handleCategoryClick('Food')}>
+					<span className='food-text'>Food</span>
+				</div>
+				<div
+					className='category-button sport-button'
+					onClick={() => handleCategoryClick('Sport')}>
+					<span className='sport-text'>Sport</span>
+				</div>
+				<div
+					className='category-button technology-button'
+					onClick={() => handleCategoryClick('Technology')}>
+					Technology
+				</div>
+				<div
+					className='category-button travel-button'
+					onClick={() => handleCategoryClick('Travel')}>
+					Travel
+				</div>
+			</div>
 		</>
-
-		
 	);
 }
 
