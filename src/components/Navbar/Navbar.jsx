4ed5010b7a61e77591398735308b9e3594e1603c
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-08-22 21:46:46 +0100

    servicesSold and servicesBought fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,76 +1,256 @@
 import { Link, useNavigate } from 'react-router-dom';
-import { useContext} from 'react';
+import { useContext } from 'react';
 import { AuthContext } from '../../context/auth.context';
 import { CartContext } from '../../context/cart.contex';
-import { ShoppingCartSimple } from 'phosphor-react';
-import './Navbar.css'
-
+import { ShoppingCartSimple, UserCircle } from 'phosphor-react';
+import './Navbar.css';
 
 function Navbar() {
 	const { user, loading, logout } = useContext(AuthContext);
 	const { cartTotalQuantity } = useContext(CartContext);
-	const navigate = useNavigate()
-	
+	const navigate = useNavigate();
+
 	const handleDiscoverServices = () => {
 		// Clear the category state, navigate and refresh
-		navigate('/main', {state: {category: ''}})
-		navigate(0)
-	}
+		navigate('/main', { state: { category: '' } });
+		navigate(0);
+	};
 
 	return (
-		<nav className='Navbar'>
-			<ul className='navbarList'>
-				<p>{user ? `Hello ${user.name}` : ''}</p>
-				{user && (
-					<img
-						className='profile-pic'
-						src={user.img}
-					/>
-				)}
+		// <nav className='Navbar'>
+		// 	<ul className='navbarList'>
 
-				
-				<button onClick={handleDiscoverServices}>Discover services</button>
-				
+		// 		<button onClick={handleDiscoverServices}>Discover services</button>
 
-				{!loading && user && (
-					<>
-						<Link to='/user-profile'>
-							<button>User profile</button>
-						</Link>
-						<button onClick={logout}>Log out</button>
-					</>
-				)}
+		// 		{!loading && user && user.typeOfUser.includes('Seller') && (
+		// 			<>
+		// 				<Link to='/add-service'>
+		// 					<button>Add service</button>
+		// 				</Link>
+		// 			</>
+		// 		)}
 
-				{!loading && user && user.typeOfUser.includes('Seller') && (
-					<>
-						<Link to='/add-service'>
-							<button>Add service</button>
-						</Link>
-					</>
-				)}
+		// 		{!loading && !user && (
+		// 			<>
+		// 				<Link to='/signup'>
+		// 					<button>Sign up</button>
+		// 				</Link>
+		// 				<Link to='/login'>
+		// 					<button>Log in</button>
+		// 				</Link>
+		// 			</>
+		// 		)}
 
-				{!loading && !user && (
-					<>
-						<Link to='/signup'>
-							<button>Sign up</button>
-						</Link>
-						<Link to='/login'>
-							<button>Log in</button>
-						</Link>
-					</>
-				)}
+		// 		{!loading && user && (
+		// 			<button onClick={logout}>Log out</button>
+		// 		)}
 
-				<div className='cart'>
-					<p>{cartTotalQuantity}</p>
+		// 		<div className='cart'>
+		// 			<p>{cartTotalQuantity}</p>
 
-					<Link to='/cart'>
-						<ShoppingCartSimple
-							size={32}
-							color='#ffffff'
-						/>
-					</Link>
+		// 			<Link to='/cart'>
+		// 				<ShoppingCartSimple
+		// 					size={32}
+		// 					color='#ffffff'
+		// 				/>
+		// 			</Link>
+		// 		</div>
+
+		// 		<p>{user ? `${user.name}` : ''}</p>
+
+		// 		{!loading && user && (
+		// 			<>
+		// 				<Link to='/user-profile'>
+		// 					<UserCircle
+		// 						size={32}
+		// 						color='#cfcfcf'
+		// 					/>
+		// 				</Link>
+		// 			</>
+		// 		)}
+		// 	</ul>
+		// </nav>
+
+		//-----------------------------------------------------------------------------------------------------------------------------------
+
+		<nav className='bg-black'>
+			<div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
+				<div className='relative flex h-16 items-center justify-between'>
+					<div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
+						{/* <!-- Mobile menu button--> */}
+						<button
+							type='button'
+							className='relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'
+							aria-controls='mobile-menu'
+							aria-expanded='false'>
+							<span className='absolute -inset-0.5'></span>
+							<span className='sr-only'>Open main menu</span>
+							{/* <!--
+							Icon when menu is closed.
+
+							Menu open: "hidden", Menu closed: "block"
+							--> */}
+							<svg
+								className='block h-6 w-6'
+								fill='none'
+								viewBox='0 0 24 24'
+								strokeWidth='1.5'
+								stroke='currentColor'
+								aria-hidden='true'>
+								<path
+									strokeLinecap='round'
+									strokeLinejoin='round'
+									d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+								/>
+							</svg>
+							{/* <!--
+            Icon when menu is open.
+
+            Menu open: "block", Menu closed: "hidden"
+          --> */}
+							<svg
+								className='hidden h-6 w-6'
+								fill='none'
+								viewBox='0 0 24 24'
+								strokeWidth='1.5'
+								stroke='currentColor'
+								aria-hidden='true'>
+								<path
+									strokeLinecap='round'
+									strokeLinejoin='round'
+									d='M6 18L18 6M6 6l12 12'
+								/>
+							</svg>
+						</button>
+					</div>
+					<div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
+						<div className='flex flex-shrink-0 items-center'>
+							<img
+								className='h-6 w-auto'
+								src='/public/assets/logo.png'
+								alt='oMERCADO'
+							/>
+						</div>
+						<div className='hidden sm:ml-6 sm:block'>
+							<div className='flex space-x-4'>
+								{/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+
+								<button
+									onClick={handleDiscoverServices}
+									className='rounded-full px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'>
+									Browse all
+								</button>
+
+								{!loading && user && user.typeOfUser.includes('Seller') && (
+									<>
+										<Link to='/add-service'>
+											<button className='rounded-full px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'>
+												Add service
+											</button>
+										</Link>
+									</>
+								)}
+
+								{!loading && !user && (
+									<>
+										<Link to='/signup'>
+											<button className='rounded-full px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'>
+												Sign up
+											</button>
+										</Link>
+										<Link to='/login'>
+											<button className='rounded-full px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'>
+												Log in
+											</button>
+										</Link>
+									</>
+								)}
+
+								{!loading && user && (
+									<button
+										className='rounded-full px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
+										onClick={logout}>
+										Log out
+									</button>
+								)}
+							</div>
+						</div>
+					</div>
+					<div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
+						<div className='cart'>
+							<p>{cartTotalQuantity}</p>
+
+							<Link to='/cart'>
+								<ShoppingCartSimple
+									size={32}
+									color='#ffffff'
+								/>
+							</Link>
+						</div>
+
+						<p>{user ? `${user.name}` : ''}</p>
+
+						{!loading && user && (
+							<>
+								<Link to='/user-profile'>
+									<UserCircle
+										size={32}
+										color='#cfcfcf'
+									/>
+								</Link>
+							</>
+						)}
+					</div>
 				</div>
-			</ul>
+			</div>
+
+			{/* <!-- Mobile menu, show/hide based on menu state. --> */}
+			<div
+				className='sm:hidden'
+				id='mobile-menu'>
+				<div className='space-y-1 px-2 pb-3 pt-2'>
+					{/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
+					<button
+						onClick={handleDiscoverServices}
+						className='rounded-md bg-black px-3 py-2 text-sm font-medium text-white'
+						aria-current='page'>
+						Browse all
+					</button>
+
+					{!loading && user && user.typeOfUser.includes('Seller') && (
+						<>
+							<Link to='/add-service'>
+								<button className='rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'>
+									Add service
+								</button>
+							</Link>
+						</>
+					)}
+
+					{!loading && !user && (
+						<>
+							<Link to='/signup'>
+								<button className='rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'>
+									Sign up
+								</button>
+							</Link>
+							<Link to='/login'>
+								<button className='rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'>
+									Log in
+								</button>
+							</Link>
+						</>
+					)}
+
+					{!loading && user && (
+						<button
+							className='rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white'
+							onClick={logout}>
+							Log out
+						</button>
+					)}
+				</div>
+			</div>
 		</nav>
 	);
 }
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,42 +1,36 @@
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useState, useEffect, useContext } from 'react';
 import { AuthContext } from '../context/auth.context';
 import UserAPIService from '../services/user.api';
 import ServicesAPIService from '../services/services.api';
 
-
 const userService = new UserAPIService();
 const servicesService = new ServicesAPIService();
-
 
 const UserProfile = () => {
 	const [servicesOffered, setServicesOffered] = useState([]);
 	const [servicesBought, setServicesBought] = useState([]);
-	const [servicesSold, setServicesSold] = useState([])
+	const [servicesSold, setServicesSold] = useState([]);
 	const [userDetails, setUserDetails] = useState({});
-
-
 
 	const { user } = useContext(AuthContext);
 
 	const getUserDetails = async () => {
 		try {
+			console.log('Hello world');
 			const response = await userService.getUserById(user._id);
-
 			setUserDetails(response.data);
 			setServicesOffered(response.data.servicesOffered);
 			setServicesBought(response.data.servicesBought);
-			console.log(response.data.servicesBought)
 			setServicesSold(response.data.servicesSold);
-			console.log('Transactions after loading data: ',response.data.servicesBought)
-
+			console.log(
+				'Transactions after loading data: ',
+				response.data.servicesSold,
+			);
 		} catch (error) {
 			console.error('Failed to fetch user details in UserProfile', error);
 		}
 	};
-
-
-
 
 	const handleDelete = async (serviceId) => {
 		try {
@@ -44,35 +38,32 @@ const UserProfile = () => {
 			// navigate('/user-profile')
 			// updating the state is faster to reload as we don't need a new API call, it's better for UI. We wold use navigate if there was a need to leave the page -> e.g. deleting user account
 			setServicesOffered((prevServices) =>
-				prevServices.filter((service) => service._id !== serviceId)
+				prevServices.filter((service) => service._id !== serviceId),
 			);
-
 		} catch (error) {
-			console.error('Failed to delete the service from UserProfile', error)
+			console.error('Failed to delete the service from UserProfile', error);
 		}
-	}
-
-
+	};
 
 	useEffect(() => {
 		getUserDetails();
 	}, []);
 
-	let typeOfUserString
+	let typeOfUserString;
 
 	if (userDetails.typeOfUser && userDetails.typeOfUser.length > 0) {
-		userDetails.typeOfUser.length === 1 ? typeOfUserString = userDetails.typeOfUser[0] : userDetails.typeOfUser.join(', ')
+		userDetails.typeOfUser.length === 1
+			? (typeOfUserString = userDetails.typeOfUser[0])
+			: userDetails.typeOfUser.join(', ');
 	}
-
 
 	return (
 		<>
-			
 			<h1>User details</h1>
 			<p>Name: {userDetails.name}</p>
 			<p>Email: {userDetails.email} </p>
 			<p>Type of account: {typeOfUserString}</p>
-			
+
 			<h1>Services Offered</h1>
 			{servicesOffered.map((service) => {
 				return (
@@ -88,10 +79,7 @@ const UserProfile = () => {
 						<Link to={`/services/edit/${service._id}`}>
 							<button>Edit</button>
 						</Link>
-						<button
-							onClick={() => handleDelete(service._id)}>
-							Delete
-						</button>
+						<button onClick={() => handleDelete(service._id)}>Delete</button>
 					</div>
 				);
 			})}
@@ -100,32 +88,33 @@ const UserProfile = () => {
 			{servicesSold.map((item) => {
 				return (
 					<div key={servicesSold.indexOf(item)}>
-					<Link to={`/services/${item.service._id}`}>
+						<Link to={`/services/${item.service._id}`}>
 							<h2>{item.service.serviceName}</h2>
 							<img
 								className='service-img'
 								src={item.service.img}></img>
-						</Link>				
-						{/* <p>Price: {item.service.unitPrice} € </p> */}
-						<p>Quantity: {item.service.quantity} </p>					
+						</Link>
+						<p>Quantity: {item.quantity} </p>
+						<p>Price: {item.service.price*item.quantity} € </p>
 					</div>
-				)
+				);
 			})}
-			
-			{/* <h1>Services Bought</h1>
+
+			<h1>Services Bought</h1>
 			{servicesBought.map((item) => {
 				return (
 					<div key={servicesBought.indexOf(item)}>
-					<Link to={`/services/${item.service._id}`}>
+						<Link to={`/services/${item.service._id}`}>
 							<h2>{item.service.serviceName}</h2>
 							<img
 								className='service-img'
 								src={item.service.img}></img>
 							<p>Quantity: {item.quantity}</p>
-						</Link>				
+							<p>Price: {item.unitPrice * item.quantity} € </p>
+						</Link>
 					</div>
-				)
-			})} */}
+				);
+			})}
 		</>
 	);
 };
