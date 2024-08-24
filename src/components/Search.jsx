715b27151057156ff6aacceb73e4ac7d00852935
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-08-24 10:55:33 +0100

    Navbar basic styles applied

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,14 +4,21 @@ const Search = (props) => {
 	};
 
 	return (
-		<form>
-			<input
-				className='search-input'
-				type='text'
-				placeholder='Search services'
-				value={props.searchValue}
-				onChange={handleSearch}></input>
-		</form>
+		
+		<div className='flex justify-center'>
+			<form className='border border-gray-400 w-3/5 rounded-s-full rounded-e-full h-12 flex mt-5'>
+				<input
+					className='w-full bg-[#0d1829] flex bg-transparent pl-2 text-[#cccccc] outline-0'
+					type='text'
+					placeholder='Search services'
+					value={props.searchValue}
+					onChange={handleSearch}>
+
+				</input>
+
+				
+			</form>
+		</div>
 	);
 };
 
