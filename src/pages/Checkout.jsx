89a946f5366ba3b814903c4f1a37aa4e5e610c1d
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-08-26 23:37:33 +0100

    Form styled but flex is not working right

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -3,6 +3,7 @@ import { useContext } from 'react';
 import { CartContext } from '../context/cart.contex';
 import { AuthContext } from '../context/auth.context';
 import TransactionAPIService from '../services/transaction.api';
+import cards from '/public/assets/cards.png';
 
 const transactionService = new TransactionAPIService();
 
@@ -26,12 +27,11 @@ function Checkout() {
 				requestBody,
 			);
 			console.log(newTransaction);
-			localStorage.removeItem('Cart')
-			localStorage.removeItem('Cart_Quantity')
-			localStorage.removeItem('Cart_Total')
+			localStorage.removeItem('Cart');
+			localStorage.removeItem('Cart_Quantity');
+			localStorage.removeItem('Cart_Total');
 			navigate('/payment-completed');
-			navigate(0)
-			
+			navigate(0);
 		} catch (error) {
 			console.error('Error when creating transaction', error);
 		}
@@ -39,38 +39,67 @@ function Checkout() {
 
 	return (
 		<>
+			<div className='bg-white text-gray-900 rounded-2xl w-1/2 m-auto mt-14 shadow-lg shadow-gray-200'>
+				
+				<form onSubmit={handleSubmit} className='w-full'>
+					<div className='flex'>
+						<div className='flex flex-col'>
+							<input
+								className='rounded-md border border-gray-300 text-sm'
+								type='string'
+								placeholder='Full name'></input>
+							<input
+								className='rounded-md border border-gray-300 text-sm'
+								type='string'
+								placeholder='Name on card'></input>
+							<input
+								className='rounded-md border border-gray-300 text-sm'
+								type='string'
+								placeholder='Expiration date (MM / YY)'
+							/>
+						</div>
 
-			<div className='summary'>
-				<h3>Summary</h3>
-				<p>Total: {totalCartSum} €</p>
-			</div>
+						<div className='flex flex-col'>	
 
-			<div className='payment-form'>
-				<form
-					className='payment-form'
-					onSubmit={handleSubmit}>
-					<input
-						type='string'
-						placeholder="Cardholder's Name"></input>
-					<input
-						type='number'
-						placeholder='Card Number'
-					/>
-					<input
-						type='string'
-						placeholder='MM/YY'
-					/>
-					<input
-						type='number'
-						placeholder='CVV'
-					/>
+							<input
+								className='rounded-md border border-gray-300 text-sm'
+								type='string'
+								placeholder='Email'></input>
+						
+							<input
+								className='rounded-md border border-gray-300 text-sm'
+								type='number'
+								placeholder='Card number'
+							/>
+						
+						
+							<input
+								className='rounded-md border border-gray-300 text-sm'
+								type='number'
+								placeholder='Security code (CVV)'
+							/>
+						
+						
+							<div className='w-1/4'>
+								<img
+									src={cards}
+									className='flex justify-center items-center'
+								/>
+							</div>
 
-					<button
-						type='submit'
-						className='payment-button'>
-						Pay
-					</button>
+							<div className='summary'>
+								<p>Total to pay: {totalCartSum} €</p>
+							</div>
+
+							<button
+								type='submit'
+								className='text-white text-sm items-center rounded-lg justify-center p-2 w-24 border bg-black hover:bg-[#9a9a9a] mt-2'>
+								Pay
+							</button>
+						</div>	
+					</div>
 				</form>
+				
 			</div>
 		</>
 	);
